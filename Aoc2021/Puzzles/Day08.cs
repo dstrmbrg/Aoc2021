e4079bca48f8b69128491ef246e5786f7a4c06cb
Daniel Strömberg <daniel.stromberg1@gmail.com>
2021-12-08 22:27:25 +0100

    Cleaner map

--- Aoc2021/Puzzles/Day08.cs
+++ Aoc2021/Puzzles/Day08.cs
@@ -21,34 +21,34 @@ namespace Aoc2021.Puzzles
                 .Sum(x => ConvertOutputSignalsToValue(x.Output, x.Map));
         }
 
-        private static int ConvertOutputSignalsToValue(string[] output, IList<(int Digit, string Pattern)> map)
+        private static int ConvertOutputSignalsToValue(string[] output, IDictionary<int, string> map)
         {
             return output.Select(p => GetOutputDigit(map, p))
                 .Select((x, i) => x * (int)Math.Pow(10, 3 - i))
                 .Sum();
         }
 
-        private static int GetOutputDigit(IList<(int Digit, string Pattern)> map, string pattern)
+        private static int GetOutputDigit(IDictionary<int, string> map, string pattern)
         {
-            return map.Single(m => m.Pattern.ToArray().ElementsEqual(pattern.ToArray())).Digit;
+            return map.Single(m => m.Value.ToArray().ElementsEqual(pattern.ToArray())).Key;
         }
 
-        private static IList<(int Digit, string Pattern)> DecipherSignalPatterns(string[] patterns)
+        private static IDictionary<int, string> DecipherSignalPatterns(string[] patterns)
         {
-            var map = new List<(int Digit, string Pattern)>
+            var map = new Dictionary<int, string>
             {
-                (1, patterns.Single(p => p.Length == 2)),
-                (4, patterns.Single(p => p.Length == 4)),
-                (7, patterns.Single(p => p.Length == 3)),
-                (8, patterns.Single(p => p.Length == 7))
+                { 1, patterns.Single(p => p.Length == 2) },
+                { 4, patterns.Single(p => p.Length == 4) },
+                { 7, patterns.Single(p => p.Length == 3) },
+                { 8, patterns.Single(p => p.Length == 7) }
             };
 
-            map.Add((3, patterns.Single(p => p.Length == 5 && map.Single(x => x.Digit == 7).Pattern.ToCharArray().All(p.Contains))));
-            map.Add((9, patterns.Single(p => p.Length == 6 && map.Single(x => x.Digit == 3).Pattern.ToCharArray().All(p.Contains))));
-            map.Add((0, patterns.Single(p => p.Length == 6 && map.None(x => x.Pattern == p) && map.Single(x => x.Digit == 7).Pattern.ToCharArray().All(p.Contains))));
-            map.Add((6, patterns.Single(p => p.Length == 6 && map.None(x => x.Pattern == p))));
-            map.Add((5, patterns.Single(p => p.Length == 5 && p.ToCharArray().All(x => map.Single(m => m.Digit == 6).Pattern.ToCharArray().Contains(x)))));
-            map.Add((2, patterns.Single(p => map.None(x => x.Pattern == p))));
+            map.Add(3, patterns.Single(p => p.Length == 5 && map[7].All(p.Contains)));
+            map.Add(9, patterns.Single(p => p.Length == 6 && map[3].All(p.Contains)));
+            map.Add(0, patterns.Single(p => p.Length == 6 && map[7].All(p.Contains) && map.None(x => x.Value == p)));
+            map.Add(6, patterns.Single(p => p.Length == 6 && map.None(x => x.Value == p)));
+            map.Add(5, patterns.Single(p => p.Length == 5 && p.All(x => map[6].Contains(x))));
+            map.Add(2, patterns.Single(p => map.None(x => x.Value == p)));
 
             return map;
         }
