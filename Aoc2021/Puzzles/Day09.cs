f9f6a12d5799242bdf211a19a7f754c4367a8bfc
Daniel Strömberg <daniel.stromberg1@gmail.com>
2021-12-09 22:02:30 +0100

    Tweaks

--- Aoc2021/Puzzles/Day09.cs
+++ Aoc2021/Puzzles/Day09.cs
@@ -6,35 +6,31 @@ namespace Aoc2021.Puzzles
 {
     internal class Day09 : Puzzle
     {
+        private static int _xMax;
+        private static int _yMax;
+
         public override object PartOne()
         {
-            var map = GetPositions().ToDictionary(p => (p.X, p.Y), p => p.Height);
-            var lowest = map.Where(p => GetAdjacentHeights(map, p.Key.X, p.Key.Y).All(a => a > p.Value));
+            var map = GetLocations().ToDictionary(p => (p.X, p.Y), p => p.Height);
+            var lowest = map.Where(p => GetAdjacentPositions(map, p.Key.X, p.Key.Y).All(a => a.Height > p.Value));
             return lowest.Sum(p => p.Value + 1);
         }
 
         public override object PartTwo()
         {
-            var map = GetPositions().ToDictionary(p => (p.X, p.Y), p => p.Height);
+            var map = GetLocations().ToDictionary(p => (p.X, p.Y), p => p.Height);
+            var lowest = map.Where(p => GetAdjacentPositions(map, p.Key.X, p.Key.Y).All(a => a.Height > p.Value));
+            var basins = lowest.Select(x => GetBasinSize(map, x.Key.X, x.Key.Y));
 
-            var lowest = map.Where(p => GetAdjacentHeights(map, p.Key.X, p.Key.Y).All(a => a > p.Value));
-
-            var basins = lowest
-                .AsParallel()
-                .Select(x => GetBasinSize(map, x.Key.X, x.Key.Y));
-
-            var largest = basins.OrderByDescending(x => x)
+            var threeLargestBasins = basins.OrderByDescending(x => x)
                 .Take(3)
                 .ToArray();
 
-            return largest.Aggregate(1, (current, basin) => current * basin);
+            return threeLargestBasins.Aggregate(1, (current, basin) => current * basin);
         }
 
-        private static IList<(int X, int Y)> GetAdjacentPositions(IDictionary<(int X, int Y), int> map, int x, int y)
+        private static IList<Location> GetAdjacentPositions(IDictionary<(int X, int Y), int> map, int x, int y)
         {
-            var xMax = map.Max(p => p.Key.X);
-            var yMax = map.Max(p => p.Key.Y);
-
             var adjacent = new List<(int X, int Y)>
             {
                 (x - 1, y),
@@ -44,21 +40,15 @@ namespace Aoc2021.Puzzles
             };
 
             return adjacent
-                .Where(a => a.X.IsBetween(0, xMax) && a.Y.IsBetween(0, yMax))
+                .Where(a => a.X.IsBetween(0, _xMax) && a.Y.IsBetween(0, _yMax))
+                .Select(a => new Location(a.X, a.Y, map[(a.X, a.Y)]))
                 .ToList();
         }
 
-        private static int[] GetAdjacentHeights(IDictionary<(int X, int Y), int> map, int x, int y)
+        private static IList<Location> GetAdjacentHigherPositions(IDictionary<(int X, int Y), int> map, int x, int y)
         {
             return GetAdjacentPositions(map, x, y)
-                .Select(a => map[(a.X, a.Y)])
-                .ToArray();
-        }
-
-        private static IList<(int X, int Y)> GetAdjacentHigherPositions(IDictionary<(int X, int Y), int> map, int x, int y)
-        {
-            return GetAdjacentPositions(map, x, y)
-                .Where(a => map[(a.X, a.Y)] != 9 && map[(a.X, a.Y)] > map[(x, y)])
+                .Where(a => a.Height != 9 && a.Height > map[(x, y)])
                 .ToList();
         }
 
@@ -69,27 +59,35 @@ namespace Aoc2021.Puzzles
             return basin.Distinct().Count() + 1;
         }
 
-        private static IList<(int X, int Y)> GetBasin(IDictionary<(int X, int Y), int> map, int x, int y)
+        private static IList<Location> GetBasin(IDictionary<(int X, int Y), int> map, int x, int y)
         {
             var adjacent = GetAdjacentHigherPositions(map, x, y);
             
             return adjacent.Concat(adjacent.SelectMany(a => GetBasin(map, a.X, a.Y))).ToList();
         }
 
-
-        private IEnumerable<(int X, int Y, int Height)> GetPositions()
+        private IEnumerable<Location> GetLocations()
         {
             var rows = Utilities.GetInput(GetType())
                 .Split(Environment.NewLine);
+
+            _yMax = rows.Length - 1;
+            _xMax = rows[0].Length - 1;
 
             for (var y = 0; y < rows.Length; y++)
             {
                 for (var x = 0; x < rows[y].Length; x++)
                 {
-                    yield return (x, y, (int)char.GetNumericValue(rows[y][x]));
+                    yield return new Location(x, y, (int)char.GetNumericValue(rows[y][x]));
                 }
-
             }
+        }
+
+        private record Location(int X, int Y, int Height)
+        {
+            public readonly int X = X;
+            public readonly int Y = Y;
+            public readonly int Height = Height;
         }
     }
 }
